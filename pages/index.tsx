92d883107098af3c7521dbc927abf039aaf750f4
Julia Dmytrenko <dmytrenkojulia@gmail.com>
2022-08-10 13:47:26 +0200

    remove unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import PizzaCard from "../components/PizzaCard";
 import styles from "../styles/Home.module.scss";
 import { pizzas } from "./../utils/pizzas";
