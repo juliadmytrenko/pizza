9b3ffa15625809836604cf6e32094e00d8c6821b
Julia Dmytrenko <dmytrenkojulia@gmail.com>
2022-08-10 19:47:24 +0200

    fix server error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  if (isSSR) return null;
   return <Component {...pageProps} />;
 }
 
